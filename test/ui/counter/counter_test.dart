eb964bd5da06b35c0e7729375a68c9fdd82a188e
Randerson <ryd@certi.org.br>
2022-03-23 19:19:35 -0300

    feat: create more test cases and remove unnecessary widget

--- test/ui/counter/counter_test.dart
+++ test/ui/counter/counter_test.dart
@@ -9,21 +9,21 @@ import 'package:simple_app/screens/counter/counter_screen.dart';
 import '../../testable_widget.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    Widget howToWidget = const CounterScreen();
+  testWidgets('Counter increments and Bottom Navigation',
+      (WidgetTester tester) async {
+    Widget counterScreen = const CounterScreen();
 
-    await tester.pumpWidget(getTestableWidget(howToWidget));
+    await tester.pumpWidget(getTestableWidget(counterScreen));
 
-    // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+
+    expect(find.byType(Icon), findsNWidgets(4));
   });
 }
